572619911b5f7d728310d5fa71821d49defd850c
miguelLopez <miguel279trabajo@gmail.com>
2021-11-14 22:20:55 -0300

    intento de edad en js

--- JS/funcionesGrupo99.js
+++ JS/funcionesGrupo99.js
@@ -5,3 +5,39 @@ function mensaje() {
 function notificacion() {
     alert ("Mensaje enviado")
 }
+
+
+/*Intento de calcular la edad*/
+const fechaNacimiento = document.getElementById(fecNac)
+const edad = document.getElementById(edad)
+const calcularEdada=(fecNac)=>{
+    const fechaActual=new Date();
+    const anioActual=parseInt(fechaActual.getFullYear());
+    const mesActual=parseInt(fechaActual.getMonth()) + 1;
+    const diaActual=parseInt(fechaActual.getDate());
+
+    const anoNacimiento=parseInt(String(fechaNacimiento).substring(0, 4))
+    const mesNacimiento=parseInt(String(fechaNacimiento).substring(5, 7))
+    const diaNacimiento=parseInt(String(fechaNacimiento).substring(8, 10))
+
+
+
+    let edad= anioActual - anoNacimiento;
+    if(mesActual<mesNacimiento){
+        edad = edad - 1;
+    }else if(mesActual==mesNacimiento){
+        if(diaActual<diaNacimiento){
+            edad--;
+        }
+    }
+    return edad;
+};
+
+window.addEventListener('load',function(){
+    fechaNacimiento.addEventListener('change',function(){
+        if(this.value){
+            edad.innerText = 'la edad es: ${calcularEdad(this.value)} años';
+        }
+        
+    })
+})
