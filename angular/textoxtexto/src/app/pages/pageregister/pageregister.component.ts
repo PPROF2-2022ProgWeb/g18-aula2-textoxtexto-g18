bf83b243a3ca36bb137c5a03857cddd33d2db867
ElGuerreroDragon <lucas25monzon@gmail.com>
2022-07-08 21:43:26 -0300

    Migracion de html a angular con enrutamiento
    Migracion de html a angular con enrutamiento dinamico, para que se convierta en un SPA

---
+++ angular/textoxtexto/src/app/pages/pageregister/pageregister.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pageregister',
+  templateUrl: './pageregister.component.html',
+  styleUrls: ['./pageregister.component.css']
+})
+export class PageregisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
