4e1dbd4d1f6660a6d89dc85bf2fcabf082c3961b
ElGuerreroDragon <lucas25monzon@gmail.com>
2022-07-31 14:44:55 -0300

    pagina en ingles
    pagina en ingles

---
+++ angular en/textoxtexto/src/app/pages/pagehome/pagehome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pagehome',
+  templateUrl: './pagehome.component.html',
+  styleUrls: ['./pagehome.component.css']
+})
+export class PagehomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular en/textoxtexto/src/app/pages/pageregister/pageregister.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageregisterComponent } from './pageregister.component';
+
+describe('PageregisterComponent', () => {
+  let component: PageregisterComponent;
+  let fixture: ComponentFixture<PageregisterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageregisterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageregisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
